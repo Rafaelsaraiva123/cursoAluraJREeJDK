f67888bd6cbbae94844c9d03df9345099a815a51
rafael saraiva <rafaelsaraiva@pop-os.localdomain>
2021-11-08 09:22:51 -0300

    Segundo Commit

---
+++ src/sintaxeBasica/TestaPontoFlutuante.java
@@ -0,0 +1,13 @@
+package sintaxeBasica;
+
+public class TestaPontoFlutuante {
+    public static void main(String[] args) {
+        double salario = 1250.74;
+        System.out.println("Meu salário é: " +salario);
+
+        double divisao= 5 / 2;
+        System.out.println(divisao);
+
+
+    }
+}
---
+++ src/sintaxeBasica/TestaVariaveis.java
@@ -0,0 +1,12 @@
+package sintaxeBasica;
+
+public class TestaVariaveis {
+    public static void main(String[] args) {
+        System.out.println("Ola novo teste");
+        int idade;
+        idade = 25;
+        System.out.println(idade);
+
+        System.out.println("A idade é: "+idade+" parabéns");
+    }
+}
